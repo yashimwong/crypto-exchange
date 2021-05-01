4a26590ac39163c5925a14158694c06f08989de5
yashimwong <yashimwong@gmail.com>
2021-05-01 13:25:58 +0800

    stream - generate large file for test

---
+++ streams/file_generator.js
@@ -0,0 +1,13 @@
+const { writeFileSync, statSync } = require("fs");
+
+// This script is to generate a large file to test with streams
+for (let i = 0; i < 10000; i++) {
+  let additional_string =
+    i > 0 ? Number(statSync("./content/large_file.txt").size) / 1000 : "";
+
+  writeFileSync(
+    "./content/large_file.txt",
+    `\nCurrent iteration: ${i} file size: ${additional_string} kB`,
+    { flag: "a" }
+  );
+}
