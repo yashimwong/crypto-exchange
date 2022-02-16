be527153cb32306db753754dd950951fa00df5c9
yashimwong <yashimwong@gmail.com>
2022-02-16 22:58:49 +0800

    feat: added wallet repository

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletController } from './wallet.controller';
+import { WalletRepository } from './wallet.repository';
 import { WalletService } from './wallet.service';
 
 @Module({
-  controllers: [WalletController],
-  providers: [WalletService]
+    imports: [TypeOrmModule.forFeature([WalletRepository])],
+    controllers: [WalletController],
+    providers: [WalletService],
 })
 export class WalletModule {}
