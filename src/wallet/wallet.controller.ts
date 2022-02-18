47f6d974e4842f6bfa93e22dbe1c9e5b0e2b8720
yashimwong <yashimwong@gmail.com>
2022-02-19 01:47:27 +0800

    feat: add wallet controller

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { WalletDTO } from './dto/create-wallet.dto';
+import { WalletBalanceDTO } from './dto/wallet-balance.dto';
+import { Wallet } from './model/wallet.entity';
 import { WalletService } from './wallet.service';
 
 @Controller('wallet')
 export class WalletController {
     constructor(private walletService: WalletService) {}
+
+    @Get('/:id')
+    async getWallets(@Param('id') account_id: string) {
+        return this.getWallets(account_id);
+    }
+
+    @Post('')
+    async addWallet(@Body() walletDTO: WalletDTO): Promise<Wallet> {
+        return this.walletService.addWallet(walletDTO);
+    }
+
+    @Post('/balance')
+    async updateBalance(@Body() walletBalanceDTO: WalletBalanceDTO): Promise<Wallet> {
+        return this.walletService.updateBalance(walletBalanceDTO);
+    }
 }
